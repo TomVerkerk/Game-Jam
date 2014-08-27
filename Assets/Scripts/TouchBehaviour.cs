361241f9b5575b6eec66243ec4825e987fa26732
TomVerkerk <Tomverkerk1@hotmail.com>
2014-08-27 12:14:36 +0200

    Touchbehaviour
    werk ochtend 27-08

---
+++ Assets/Scripts/TouchBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchBehaviour : MonoBehaviour {
+
+	private Vector2 touch;
+
+	// Use this for initialization
+	void Start () {
+		Input.multiTouchEnabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (var T in Input.touches) {
+			if(T.phase == TouchPhase.Moved){
+				transform.position = T.position;
+			}
+		}
+	}
+}
